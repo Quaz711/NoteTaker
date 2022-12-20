e5564227f08faec3e851404f3c3a831cb8ec31b2
Anthony <quaz711@hotmail.com>
2022-12-20 16:23:30 -0500

    Added server.js, apiRoutes.js, and htmlRoutes.js

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => {
+    console.log(`PORT: ${PORT} is being used.`)
+});
