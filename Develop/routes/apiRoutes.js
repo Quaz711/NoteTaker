3a65a4d49695568ade8d76a59c9a99f1c7e7d78d
Anthony <quaz711@hotmail.com>
2023-01-09 18:21:24 -0500

    added router and changed up noets.html

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,12 +3,13 @@ const path = require("path");
 const data = require("../db/db.json");
 const {v4:uuivd4} = require("uuid");
 
-module.exports = app => {
-    app.get("/api/notes", function (req, res) {
-        res.json(data);
+module.exports = apply => {
+    apply.get("/api/notes", function (req, res) {
+        res.sendFile(path.join(__dirname, "../db/db.json"))
+        //res.json(data);
     });
 
-    app.post("/api/notes", function (req, res) {
+    apply.post("/api/notes", function (req, res) {
         let newNote = req.body;
         newNote.id = uuidv4();
         data.push(newNote);
@@ -16,7 +17,7 @@ module.exports = app => {
         res.json(data);
     });
 
-    app.delete("/api/notes/:id", function (req, res) {
+    apply.delete("/api/notes/:id", function (req, res) {
         let noteID =req.params.id;
         const newInfo = data.filter(note => note.id !== noteID);
         fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(newInfo));
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const htmlRoutes = require("./Develop/routes/htmlRoutes");
+const apiRoutes = require("./Develop/routes/apiRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(express.static("public"));
-
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.use(express.static(__dirname + '/public'));
+app.use("/", htmlRoutes);
+app.use("/api", apiRoutes);
 
 app.listen(PORT, function () {
     console.log(`PORT: ${PORT} is being utilized.`)
